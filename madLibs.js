93ff979548735977d5f3640e1ca08cfe76912e9b
Mimi <trish21215@yahoo.com>
2025-03-26 11:05:38 -0400

    chose story template and replaced story with placeholders

---
+++ madLibs.js
@@ -0,0 +1,5 @@
+const ghoul = 'ghost'
+const scary ='haunted'
+const creepyFeeling ='Spooky'
+
+`'On a stormy night, I ventured into the ${haunted} house. The creaking walls seemed to whisper, sending chills down my spine. Suddenly, a ${ghost} appeared, its glowing eyes piercing through the darkness. My heart raced as it floated silently toward me. Did you know 10 - 7 equals 3? ${Spooky}, right?'`
